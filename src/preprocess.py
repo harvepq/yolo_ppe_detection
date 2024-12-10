e3826c37f6a19e1f1d33ad086076743bd12e7a0e
Harvey <harvepq@gmail.com>
2024-12-10 16:58:53 -0500

    Add resizing functionality and improve preprocess.py file

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,14 @@
+# Import libraries
+import os
+import shutil
+
 from datetime import datetime
 from PIL import Image, ImageOps
-import os
 
-from config import RAW_DATA_DIR, INTERIM_DATA_DIR
+from config import DATA_DIR, RAW_DATA_DIR, INTERIM_DATA_DIR
 
-def main():
+# Main section
+def main(resize=True):
     # Initialize variables
     image_counter = 0
     processed_dirs = []
@@ -15,49 +19,71 @@ def main():
 
     # Processed file name and path
     processed_file = 'processed.txt'
-    processed_file_path = os.path.join(RAW_DATA_DIR, processed_file)
+    processed_file_path = os.path.join(DATA_DIR, processed_file)
 
     # Create a new file and open to append content
-    if processed_file not in raw_data_dirs:
+    if processed_file not in os.listdir(DATA_DIR):
         file = open(processed_file_path, '+a')
     else:
         file = open(processed_file_path, 'r+')
         # Get the processed dirs names
-        processed_dirs = file.read().splitlines()
+        file_list = file.read().splitlines()
+        processed_dirs = file_list[:-2]
 
-        if len(processed_dirs) > 1:
+        if len(processed_dirs) > 0:
             # Get the number of processed images
-            image_counter = int(processed_dirs[-2])
-            time_processed = int(processed_dirs[-1])
+            image_counter = int(file_list[-2])
+            time_processed = int(file_list[-1])
 
         # Delete the last line of the file
         file.seek(0)
         file.truncate()
-        for dir in processed_dirs[:-2]:
+        for dir in processed_dirs:
             file.write(f'{dir}\n')
 
-    # Add time processed
-    time_processed += 1
+    # Verify if there are any raw data to process
+    dirs_to_process = [e for e in raw_data_dirs if os.path.isdir(os.path.join(RAW_DATA_DIR, e)) and e not in processed_dirs]
 
-    # Create new folder
-    current_day = datetime.now().strftime('%Y_%m_%d')
-    folder_name = f'{time_processed:02}_{current_day}'
-    interim_folder_path = os.path.join(INTERIM_DATA_DIR, folder_name)
-    os.makedirs(interim_folder_path, exist_ok=True)
+    if len(dirs_to_process) == 0:
+        print('There are not any data to process')
+    else:
+        print('There are raw data to process')
+        # Add time processed
+        time_processed += 1
 
-    # Read all images dirs from raw data dir
-    for dir in raw_data_dirs:
-        # Verify if some dirs was processed
-        if dir not in processed_dirs:
+        # Create new folder
+        current_day = datetime.now().strftime('%Y_%m_%d')
+        folder_name = f'{time_processed:02}_{current_day}'
+        interim_folder_path = os.path.join(INTERIM_DATA_DIR, folder_name)
+        os.makedirs(interim_folder_path, exist_ok=True)
+
+        # Read all images dirs from raw data dir
+        for dir in dirs_to_process:
             raw_data_dir = os.path.join(RAW_DATA_DIR, dir)
 
-            # Verify if is directory
-            if os.path.isdir(raw_data_dir):
+            raw_images_name = os.listdir(raw_data_dir)
 
-                # Read all images
-                for image_name in os.listdir(raw_data_dir):
-                    image_path = os.path.join(raw_data_dir, image_name)
+            # Read all images
+            for image_name in raw_images_name:
 
+                # Increase image counter
+                image_counter += 1
+
+                # Image path
+                image_path = os.path.join(raw_data_dir, image_name)
+
+                # Create image name
+                new_image_name = f'{image_counter:06}.jpg'
+
+                # Create image path
+                new_image_path = os.path.join(interim_folder_path, new_image_name)
+
+                # Copy images without resizing
+                if not resize:
+                    shutil.copyfile(image_path, new_image_path)
+
+                # Resize images
+                else:
                     # Read the image
                     image = Image.open(image_path)
                     # Image dimensions
@@ -87,20 +113,11 @@ def main():
                         # Add padding with white color (255, 255, 255)
                         image = ImageOps.expand(image, border=(pd_left, pd_top, pd_right, pd_bottom), fill="white")
 
-                    # Increase image counter
-                    image_counter += 1
-
-                    # Create image name
-                    new_image_name = f'{image_counter:06}.jpg'
-
-                    # Create image path
-                    new_image_path = os.path.join(interim_folder_path, new_image_name)
-
                     # Save the new image into intermediate data
                     image.save(new_image_path)
 
-                # Append dir into processed file
-                file.write(f'{dir}\n')
+            # Append dir into processed file
+            file.write(f'{dir}\n')
 
     # Append the image counter and time processed into processed file
     file.write(f'{image_counter}\n')
@@ -110,4 +127,12 @@ def main():
     file.close()
 
 if __name__ == "__main__":
-    main()
+    resize = True
+    print('Enter one option')
+    print('0. Dont resize images')
+    print('1. Resize images to 640 x 640')
+    op = int(input('Option: '))
+    if op == 0:
+        resize = False
+
+    main(resize=resize)
