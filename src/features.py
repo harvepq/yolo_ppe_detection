801b6d2c4f6b3bd9e6acab2d5d19b42f6a2174b2
Harvey <harvepq@gmail.com>
2024-12-11 00:59:52 -0500

    Modify feature extraction to modify dataset data.yaml file

--- src/features.py
+++ src/features.py
@@ -1,29 +1,44 @@
-from pathlib import Path
-
+# Import libraries
+import os
 import typer
-from loguru import logger
-from tqdm import tqdm
+import yaml
 
-from src.config import PROCESSED_DATA_DIR
+from config import DATASET_DIR
+from loguru import logger
 
 app = typer.Typer()
 
-
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
+def main():
+    # data yaml path
+    data_yaml_path = os.path.join(DATASET_DIR, 'data.yaml')
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    # Open the file
+    with open(data_yaml_path, 'r+') as file:
+        try:
+            # Read the data yaml content
+            data = yaml.safe_load(file)
+
+            # Clear file
+            file.seek(0)
+            file.truncate()
+
+            # Modify the train, test and validation path
+            data['train'] = os.path.join(DATASET_DIR, 'train', 'images')
+            data['test'] = os.path.join(DATASET_DIR, 'test', 'images')
+            data['val'] = os.path.join(DATASET_DIR, 'val', 'images')
+
+            # Save the modified content
+            yaml.dump(data, file)
+
+        except yaml.YAMLError as e:
+            print('Error reading YAML:', e)
+    # Close the file
+    file.close()
+
     logger.success("Features generation complete.")
     # -----------------------------------------
-
 
 if __name__ == "__main__":
     app()
